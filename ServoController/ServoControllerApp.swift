3754ef7996637f5becc06efa55eee6e31833ce99
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-04-25 17:55:47 +0200

    Initial Commit

---
+++ ServoController/ServoControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServoControllerApp.swift
+//  ServoController
+//
+//  Created by Stjepan Stojčević on 25.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct ServoControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

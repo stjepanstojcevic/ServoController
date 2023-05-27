5d242932ab87c622f20a7d78711bf7c3ec0bf1b3
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-05-28 01:36:29 +0200

    Dodavanje buttona, gradijentne boje i još malo estetiziranja

--- ServoController/Model.swift
+++ ServoController/Model.swift
@@ -16,17 +16,36 @@ struct Model: View {
     
     var body: some View {
         ZStack {
-            Color(.systemGreen).edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [Color.green.opacity(0.3), Color.green]), startPoint: .top, endPoint: .bottom)
+                            .edgesIgnoringSafeArea(.all)
             VStack{
                 Spacer()
-                Text("Servo motor degrees").font(.title)
-                Slider(value: $value, in: 0...360)
-                Text("\(value, specifier: "%.f")")
+                Text("Bluetooth Servo Motor Controller").font(.title).bold()
+                Spacer()
+                Slider(value: $value, in: 0...360).accentColor(.black)
+    
+                Spacer()
+                VStack(spacing: 20){
+                    Button(action: self.set(num: 45.0),label: {Text(" Postavi na  45  stupnjeva ")}).foregroundColor(.green).frame(width: 200, height: 40).background(Color.black)
+                        .cornerRadius(10)
+                    Button(action: self.set(num:60.0),label: {Text(" Postavi na  60  stupnjeva ")}).foregroundColor(.green).frame(width: 200, height: 40).background(Color.black)
+                        .cornerRadius(10)
+                    Button(action: self.set(num: 90.0),label: {Text(" Postavi na  90  stupnjeva ")}).foregroundColor(.green).frame(width: 200, height: 40).background(Color.black)
+                        .cornerRadius(10)
+                    Button(action: self.set(num:180.0),label: {Text(" Postavi na 180 stupnjeva")}).foregroundColor(.green).frame(width: 200, height: 40).background(Color.black)
+                        .cornerRadius(10)
+                }
+                Spacer()
+                Spacer()
+                Text("Okreni servo motor za \(value, specifier: "%.f") stupnjeva").font(.title2).bold()
                 Spacer()
             }
         }
     }
-    
+    func set(num: Double) -> () -> Void
+    {
+        return {value=num}
+    }
 }
 
             
